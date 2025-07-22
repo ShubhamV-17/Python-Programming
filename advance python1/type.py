cf8e85e094e5a71006b79bfc9837c3b864719aa1
Shubham Vishwakarma <shubham10137@gmail.com>
2025-07-22 23:55:07 +0530

    intial commit

---
+++ advance python1/type.py
@@ -0,0 +1,10 @@
+n : int = 5
+
+name: str = "shubham"
+
+
+
+
+def sum(a:int,b:int) -> int:
+    return a+b
+
---
+++ advance python1/walurus.py
@@ -0,0 +1,3 @@
+# using walurus operator
+if n := len([1,2,3,4,5]) > 3:
+    print(f"list is to long ({n} elements , expected <=3)")
